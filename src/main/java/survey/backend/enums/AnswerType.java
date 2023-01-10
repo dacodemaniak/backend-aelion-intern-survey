ef714252ca255ef5bf8a1321c29c456b14a1e948
matthcol <matthias.colin@gmail.com>
2023-01-10 14:36:32 +0100

    enum types for survey

---
+++ src/main/java/survey/backend/enums/AnswerType.java
@@ -0,0 +1,8 @@
+package survey.backend.enums;
+
+public enum AnswerType {
+    YES_NO,
+    CHOOSE_ONE,
+    CHOOSE_MANY,
+    FREE
+}
---
+++ src/main/java/survey/backend/enums/Level.java
@@ -0,0 +1,5 @@
+package survey.backend.enums;
+
+public enum Level {
+    ONE_MONTH, SIX_MONTHS, ONE_YEAR
+}
